8b7015b779697a2fd35305884466631e146e30e0
Muhammed Ali <muhammedalibalci@gmail.com>
2020-10-23 11:42:57 +0300

    Added backend .Net Core 3.1

---
+++ Backend/InfoHub/Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoHub.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkObjectResult("TEST");
+        }
+    }
+}
